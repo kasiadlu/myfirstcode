5a7e0053208974dce14334098f511e2ecbed4901
kasiadlu <52886401+kasiadlu@users.noreply.github.com>
2019-08-01 16:34:49 +0200

    Create kosiammisiolinkita.c

---
+++ kosiammisiolinkita.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void) {
+    
+    int number = get_int("Enter a number between 0 and 2: ");
+    
+    if (number == 1) {
+        printf("Wesołych Świąt!\n");
+    } else if (number == 2) {
+        printf("Kosiam Misiolinka!\n");
+    } else if (number == 0) {
+        exit(0);
+    } else {
+        main();
+    }
+}
