245e8f88d8915ae7031d9e25fcf6cb1731c32051
kasiadlu <52886401+kasiadlu@users.noreply.github.com>
2019-08-02 19:47:15 +0200

    Update kosiammisiolinkita.c

--- kosiammisiolinkita.c
+++ kosiammisiolinkita.c
@@ -3,15 +3,15 @@
 
 int main(void) {
     
-    int number = get_int("Enter a number between 0 and 2: ");
+    int number = 0;
     
-    if (number == 1) {
-        printf("Wesołych Świąt!\n");
-    } else if (number == 2) {
-        printf("Kosiam Misiolinka!\n");
-    } else if (number == 0) {
-        exit(0);
-    } else {
-        main();
-    }
+    do {
+        number = get_int("Enter a number between 0 and 2: ");
+        if (number == 1) {
+            printf("Wesołych Świąt!\n");
+        } else if (number == 2) {
+            printf("Kosiam Misiolinka!\n");
+        }
+    } while (number != 0);
+        
 }
