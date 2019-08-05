a0fcb5a1a9c39ad4ac0daa9356e3a972c745e25e
kasiadlu <52886401+kasiadlu@users.noreply.github.com>
2019-08-05 22:26:59 +0200

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <stdlib.h>
+#include <string.h>
 
 int strLen(string s);
 bool isInteger(string dupa);
+string changeToCiphertext(string plaintext, int k);
 
 int main(int argc, string argv[])
 {
     
     if (argc == 2 && isInteger(argv[1])) {
         int k = atoi(argv[1]);
-        printf("Success\n");
-        printf("%i\n", k);
+        string plaintext = get_string("plaintext: ");
+        string ciphertext = changeToCiphertext(plaintext, k);
+        printf("ciphertext: %s\n", ciphertext);
     } else {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -39,4 +42,24 @@ bool isInteger(string dupa) {
         }
     }
     return true;
+}
+
+string changeToCiphertext(string plaintext, int k) {
+    int lentgh = strLen(plaintext);
+    for (int i = 0; i < lentgh; i++) {
+        int keyAdded = plaintext[i] + k;
+        if (keyAdded > 122) {
+            int leftReminder = 122 - plaintext[i];
+            int rightReminder = k - leftReminder - 1;
+            plaintext[i] = 97 + rightReminder;
+        } else if (keyAdded > 90) {
+            int leftReminder = 90 - plaintext[i];
+            int rightReminder = k - leftReminder - 1;
+            plaintext[i] = 65 + rightReminder;
+        } else {
+            plaintext[i] = keyAdded;
+        }
+//         
+    }
+    return plaintext;
 }
