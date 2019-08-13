8fb596a7baa345fefe16ab9b31fee66d740ee89a
kasiadlu <52886401+kasiadlu@users.noreply.github.com>
2019-08-13 19:54:13 +0200

    Create cEx_1_20.c

---
+++ cEx_1_20.c
@@ -0,0 +1,41 @@
+/*
+ * main.c
+ *
+ *  Created on: 10 sie 2019
+ *      Author: kbdlu
+ */
+
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+#include "math.h"
+
+void program();
+
+#define SIZE 5
+
+int main(void) {
+	do {
+		string command = get_string("Input \'y\' to open a program, anything other for exit: ");
+		if (command[0] == 'y') {
+			program();
+		} else {
+			return 0;
+		}
+	} while (true);
+}
+
+void program() {
+	int array[SIZE] = {};
+	for(int i = 0; i < SIZE; i++) {
+		array[i] = get_int("Enter a number: ");
+		}
+	printf("Array values are:\n");
+	for(int i = 0; i < SIZE; i++) {
+		if(array[i] <= 0) {
+			array[i] = 100;
+		}
+		printf("n[%d] = %d\n", i, array[i]);
+	}
+}
+
